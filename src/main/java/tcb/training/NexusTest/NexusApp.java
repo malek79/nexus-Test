c905198d8dd52e968803ce71d8710a259d193795
malek79 <ben.othmen.malek@gmail.com>
2018-04-06 10:33:11 +0100

    nexus test

---
+++ src/main/java/tcb/training/NexusTest/NexusApp.java
@@ -0,0 +1,15 @@
+package tcb.training.NexusTest;
+
+import tcb.training.JenkinsTest.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class NexusApp 
+{
+    public static void main( String[] args )
+    {
+        App.main(args);
+    }
+}
